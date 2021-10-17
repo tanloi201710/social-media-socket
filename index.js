3885ea1a8ffb5ec1a6523423caa362edbf48772f
TanLoi <loihaag12345@gmail.com>
2021-10-17 18:51:17 +0700

    initial and first feature

---
+++ index.js
@@ -0,0 +1,40 @@
+const io = require('socket.io')(8080, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
+
+let users = [];
+
+const addUser = (userId,socketId) => {
+    !users.some(user => user.id === userId) && users.push({ userId, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+}
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+    // add user from client socket
+    socket.on('addUser', (userId) => {
+        addUser(userId,socket.id);
+        io.emit('getUsers', users);
+    });
+
+    // send and get message
+    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit('getMessage', { senderId, text });
+    });
+
+    socket.on('disconnect', () => {
+        console.log("a user disconnected");
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    });
+});
